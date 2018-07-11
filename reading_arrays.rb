186bd357bc73f8657451c7bd469bf62d96cf3474
erinawidjaja <erina.widjaja@gmail.com>
2018-07-11 16:21:04 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -10,23 +10,29 @@ STUDENT_NAMES = [
 def first_student_by_index
  STUDENT_NAMES[0]
 end
+first_student_by_index
 
 def fourth_student_by_index
   STUDENT_NAMES[3]
 end
+fourth_student_by_index
 
 def last_student_by_index
 STUDENT_NAMES[5]
 end
+last_student_by_index
 
 def first_student_by_method
 STUDENT_NAMES.first
 end
+first_student_by_method
 
 def last_student_by_method
  STUDENT_NAMES.last
 end
+last_student_by_method
 
 def first_second_and_third_students
   STUDENT_NAMES.first_second_and_third_students
 end
+first_second_and_third_students
